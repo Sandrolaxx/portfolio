e27d0999e0bc70b2cfd874027924579ce34fee0b
Sandrolaxx <sandro_pauloco@hotmail.com>
2020-07-27 21:18:49 -0300

    night commit

---
+++ scripts/scripts.js
@@ -0,0 +1,17 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+
+const cards = document.querySelectorAll('.card');
+
+
+for (let card of cards) {
+    card.addEventListener('click', function (){
+        const video = card.getAttribute("id");
+        modalOverlay.classList.add('active');
+        modalOverlay.querySelector('iframe').src = `https://www.youtube.com/embed/${video}`;
+    })
+}
+
+document.querySelector('.close-modal').addEventListener('click', function () {
+    modalOverlay.classList.remove('active');
+    modalOverlay.querySelector('iframe').src = "";
+})
